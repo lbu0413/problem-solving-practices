fc197bdfce89b93402cfab981ada45691e94001f
LBU <noeliden1@gmail.com>
2021-04-17 00:55:19 -0400

    Q19 solution

--- main.js
+++ main.js
@@ -360,3 +360,35 @@ function solution18(arr18){
 }
 let arr18 = [7, 3, 9, 5, 6, 12];
 console.log(solution18(arr18));
+
+// 19.  보이는 학생
+
+// - 선생님이 N(1<=N<=1000)명의 학생을 일렬로 세웠습니다. 일렬로 서 있는 학생의 키가 앞에
+// 서부터 순서대로 주어질 때, 맨 앞에 서 있는 선생님이 볼 수 있는 학생의 수를 구하는 프로그
+// 램을 작성하세요. (앞에 서 있는 사람들보다 크면 보이고, 작거나 같으면 보이지 않습니다.)
+// - input example: [130, 135, 148, 140, 145, 150, 150, 153]
+// - output example: 5
+
+// function solution19(arr19) {
+//     let answer = 0;
+//     for(let i = 0; i < arr19.length; i++){
+//         if(arr19[i+1] > arr19[i]) answer++;
+//     }
+//     return answer;
+// }
+// let arr19 = [130, 135, 148, 140, 145, 150, 150, 153];
+// console.log(solution19(arr19));
+
+function solution19(arr19) {
+    let answer = 1;
+    let max = arr19[0];
+    for(let i = 1; i < arr19.length; i++){
+        if(arr19[i] > max){
+            max = arr19[i];
+            answer++;
+        }
+    }
+    return answer;
+}
+let arr19 = [130, 135, 148, 140, 145, 150, 150, 153];
+console.log(solution19(arr19));
