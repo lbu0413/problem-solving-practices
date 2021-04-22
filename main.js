066d062b2f664c73f40a4e53c118a4f7b3256d18
LBU <noeliden1@gmail.com>
2021-04-22 02:03:46 -0400

    Q24 solved

--- main.js
+++ main.js
@@ -486,3 +486,49 @@ let input2 = [1, 2, 3, 4, 5];
 input2.unshift("start");
 input2.push("end");
 console.log(input2);
+
+// 24. 격자판 최대합
+
+// - 5 x 5 격자판에 아래와 같이 숫자가 적혀있습니다.
+// - N x N 의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가장 큰 합을 출력하세요.
+// - input example:
+
+//     10 13 10 12 15
+//     12 39 30 23 11
+//     11 25 50 53 15
+//     19 27 29 37 27
+//     19 13 30 13 19
+
+// - output example: 155
+
+function solution24(arr24) {
+    let answer = Number.MIN_SAFE_INTEGER;
+    let n = arr24.length;
+    let sum1 = 0;
+    let sum2 = 0;
+    for(let i = 0; i < n; i++){
+            sum1 = 0;
+            sum2 = 0;
+        for(let j = 0; j < n; j++){
+            sum1 += arr24[i][j];
+            sum2 += arr24[j][i];
+        }
+        answer = Math.max(answer, sum1, sum2);
+    }
+    sum1 = 0;
+    sum2 = 0;
+    for(let i = 0; i < n; i++) {
+        sum1 += arr24[i][i];
+        sum2 += arr24[i][n-i-1]
+    }
+    answer = Math.max(answer, sum1, sum2);
+    return answer;
+}
+
+let arr24 = 
+[[10, 13, 10, 12, 15], 
+[12, 39, 30, 23, 11],
+[11, 25, 50, 53, 15],
+[19, 27, 29, 37, 27],
+[19, 13, 30, 13, 19]];
+console.log(solution24(arr24));
