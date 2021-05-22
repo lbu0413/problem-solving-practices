9982b1c8cdf1a34cd93f8a95c15887f13fa7a3ac
LBU <vvookly@gmail.com>
2021-05-22 18:19:09 -0400

    S5-Q5-SOLUTION

--- section5.js
+++ section5.js
@@ -255,3 +255,53 @@ function solution7(e, f) {
 let e = "AbaAeCe";
 let f = "baeeACA";
 console.log("solution7: ", solution7(e, f));
+
+// 모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우)
+// S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램을 작성하
+// 세요. 아나그램 판별시 대소문자가 구분됩니다. 부분문자열은 연속된 문자열이어야 합니다.
+// ▣ 입력설명
+// 첫 줄에 첫 번째 S문자열이 입력되고, 두 번째 줄에 T문자열이 입력됩니다.
+// S문자열의 길이는 10,000을 넘지 않으며, T문자열은 S문자열보다 길이가 작거나 같습니다.
+// ▣ 출력설명
+// S단어에 T문자열과 아나그램이 되는 부분문자열의 개수를 출력합니다.
+// ▣ 입력예제 1
+// bacaAacba
+// abc
+// ▣ 출력예제 1
+// 3
+// 출력설명: {bac}, {acb}, {cba} 3개의 부분문자열이 "abc"문자열과 아나그램입니다.
+function compareMaps(map1, map2) {
+	if (map1.size !== map2.size) return false;
+	for (let [key, value] of map1) {
+		if (!map2.has(key) || map2.get(key) !== value) return false;
+	}
+	return true;
+}
+function solution8(g, h) {
+	let answer = 0;
+	let gH = new Map();
+	let hH = new Map();
+	for (let i of h) {
+		if (hH.has(i)) hH.set(i, hH.get(i) + 1);
+		else hH.set(i, 1);
+	}
+	let len = h.length - 1;
+	for (let i = 0; i < len; i++) {
+		if (gH.has(g[i])) gH.set(g[i], gH.get(g[i]) + 1);
+		else gH.set(g[i], 1);
+	}
+	let left = 0;
+	for (let right = len; right < g.length; right++) {
+		if (gH.has(g[right])) gH.set(g[right], gH.get(g[right]) + 1);
+		else gH.set(g[right], 1);
+		if (compareMaps(gH, hH)) answer++;
+		gH.set(g[left], gH.get(g[left]) - 1);
+		if (gH.get(g[left]) === 0) gH.delete(g[left]);
+		left++;
+	}
+	return answer;
+}
+
+let g = "bacaAacba";
+let h = "abc";
+console.log("solution8: ", solution8(g, h));
