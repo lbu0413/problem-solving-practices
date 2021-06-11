c83f0b5bfaf1b9b99e91d947ed0cf143a34bdf32
LBU <vvookly@gmail.com>
2021-06-11 03:34:01 -0400

    S3-Q5-SOLUTION

--- section3.js
+++ section3.js
@@ -115,3 +115,31 @@ function solution4(s, t) {
 let str4 = "teachermode";
 let t = "e";
 console.log("solution4: ", solution4(str4, t));
+
+// 문자열 압축
+
+// 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는 문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오.
+// 단 반복횟수가 1인 경우 생략합니다.
+
+// 입력예제1
+// KKHSSSSSSSE
+// 출력예제1
+// K2HS7E
+
+function solution5(s) {
+	let count = 1;
+	let answer = "";
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] === s[i + 1]) {
+			count++;
+		} else {
+			answer += s[i];
+			if (count > 1) answer += String(count);
+			count = 1;
+		}
+	}
+	return answer;
+}
+
+let str5 = "KKHSSSSSSSE";
+console.log("solution5: ", solution5(str5));
