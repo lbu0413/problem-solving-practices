b84afead83b0eee853733a5ba7940f0b43334963
LBU <vvookly@gmail.com>
2021-06-19 17:35:43 -0400

    S4-Q3-SOLUTION-REPEAT

--- REPEAT/section4-repeat.js
+++ REPEAT/section4-repeat.js
@@ -45,3 +45,33 @@ function solution2(a) {
 
 let a2 = [32, 55, 62, 20, 250, 370, 200, 30, 100];
 console.log("solution2: ", solution2(a2));
+
+function solution3(a, b) {
+	let tests = a.length;
+	let students = a[0].length;
+	let answer = 0;
+	for (let i = 1; i <= students; i++) {
+		for (let j = 1; j <= students; j++) {
+			let mentor = (mentee = 0);
+			let count = 0;
+			for (let k = 0; k < tests; k++) {
+				for (let s = 0; s < students; s++) {
+					if (a[k][s] === i) mentor = s;
+					if (a[k][s] === j) mentee = s;
+				}
+				if (mentor < mentee) count++;
+			}
+			if (count === b) answer++;
+		}
+	}
+	return answer;
+}
+
+let a3 = [
+	[3, 4, 1, 2],
+	[4, 3, 2, 1],
+	[3, 1, 4, 2],
+];
+
+let b3 = 3;
+console.log("solution3: ", solution3(a3, b3));
