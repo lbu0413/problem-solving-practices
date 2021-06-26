def388db73f74fff07441b4c0172923eb88ee262
LBU <vvookly@gmail.com>
2021-06-25 22:36:24 -0400

    S1-Q15-SOLUTION-REPEAT-2

--- REPEAT-2/section1-repeat-2.js
+++ REPEAT-2/section1-repeat-2.js
@@ -12,3 +12,19 @@ function solution13(a) {
 
 let a13 = "StuDY";
 console.log("solution13: ", solution13(a13));
+
+function solution15(a) {
+	let mid = Math.floor(a.length / 2);
+	if (a.length % 2 === 0) {
+		answer = a
+			.split("")
+			.splice(mid - 1, 2)
+			.join("");
+	} else {
+		answer = a.split("").splice(mid, 1).join("");
+	}
+	return answer;
+}
+
+let a15 = "good";
+console.log("solution15: ", solution15(a15));
